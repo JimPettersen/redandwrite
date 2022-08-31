d3388400e9591d17f8b194d343eed64dbd3c60a4
JimPettersen <91867936+JimPettersen@users.noreply.github.com>
2022-08-31 14:08:52 +0200

    Added namespace Lib
    inludes template print  :)

--- redandwrite/maincpp.cpp
+++ redandwrite/maincpp.cpp
@@ -1,4 +1,23 @@
-int main()
+#include <iostream>
+
+namespace Lib 
 {
 
+	
+
+	template <typename T>
+	void print(T t)
+	{
+		std::cout << t << "\n";
+	}
+
+
+}
+
+
+
+
+int main()
+{
+	Lib::print("hej");
 }
